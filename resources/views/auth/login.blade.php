51fd3736eeda66548017e0f0fb9907dbc2e5da64
DESKTOP-P4LCL1B\BAGAS-PC <bagas.aja20155@gmail.com>
2023-12-08 18:31:42 +0700

    layouting unverified user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="{{ asset('images/logo_ki2.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="{{ asset('images/logo_ki2.png') }}" alt="logo" width="100" >
             </div>
 
             <div class="card card-primary">
@@ -44,7 +44,7 @@
                   <div class="form-group">
                     <div class="custom-control custom-checkbox">
                       <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember_me">
-                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                      <label class="custom-control-label" for="remember_me">Remember Me</label>
                     </div>
                   </div>
 
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,21 +1,26 @@
-<div class="main-sidebar sidebar-style-2">
-    <aside id="sidebar-wrapper">
-      <div class="sidebar-brand">
-        <a href="index.html">Stisla</a>
-      </div>
-      <div class="sidebar-brand sidebar-brand-sm">
-        <a href="index.html">St</a>
-      </div>
-      <ul class="sidebar-menu">
-        <li class="menu-header">Dashboard</li>
-        <li class="nav-item dropdown">
-          <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-          <ul class="dropdown-menu">
-            <li><a class="nav-link" href="#">General Dashboard</a></li>
-            <li><a class="nav-link" href="#">Ecommerce Dashboard</a></li>
-          </ul>
-        </li>
-        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
-    </ul>    
-    </aside>
-</div>
+@if (auth()->user()->email_verified_at)
+    <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{ route('dashboard') }}"><img src="{{ asset('images/logo_ki2.png') }}" alt="Pendaftaran Merk" width="50px"></a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="{{ route('dashboard') }}"><img src="{{ asset('images/logo_ki2.png') }}" alt="Pendaftaran Merk" width="30px"></a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            
+            {{-- administrator menus --}}
+            <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            <ul class="dropdown-menu">
+                <li><a class="nav-link" href="#">General Dashboard</a></li>
+                <li><a class="nav-link" href="#">Ecommerce Dashboard</a></li>
+            </ul>
+            </li>
+            <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            {{-- ------- --}}
+        </ul>    
+        </aside>
+    </div>
+@endif
