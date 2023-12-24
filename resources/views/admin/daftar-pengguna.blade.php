8ddd522fda6e389d851ba63b9fdb3e63593fb6fd
DESKTOP-P4LCL1B\BAGAS-PC <bagas.aja20155@gmail.com>
2023-12-24 19:21:53 +0700

    export table to excel

--- resources/views/admin/daftar-pengguna.blade.php
+++ resources/views/admin/daftar-pengguna.blade.php
@@ -5,6 +5,7 @@
 @push('css')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
     <link rel=”stylesheet” href=" https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 @endpush
 
 @section('content')
@@ -42,7 +43,15 @@
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>
-                                        @if ( $user->hasRole('admin'))
+                                        {{-- {{ dd($user->hasRole('applicant')) }} --}}
+                                        @if ($user->hasRole('applicant') && $user->hasRole('admin'))
+                                            <span class="badge badge-pill badge-warning">
+                                                Pemohon
+                                            </span>
+                                            <span class="badge badge-pill badge-danger">
+                                                Admin
+                                            </span>
+                                        @elseif ( $user->hasRole('admin'))
                                             <span class="badge badge-pill badge-danger">
                                                 Admin
                                             </span>
@@ -74,6 +83,11 @@
 
 @push('js')
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
@@ -83,6 +97,10 @@
         $("#datatable").dataTable({
             "columnDefs": [
                 { "sortable": true, "targets": [0] }
+            ],
+            'dom': "Bfrtip",
+            'buttons': [
+                'excel',
             ]
         });
     </script>
--- resources/views/admin/daftar-permohonan.blade.php
+++ resources/views/admin/daftar-permohonan.blade.php
@@ -5,6 +5,7 @@
 @push('css')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
     <link rel=”stylesheet” href=" https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
 @endpush
 
 @section('content')
@@ -78,6 +79,11 @@
 
 @push('js')
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
@@ -87,6 +93,10 @@
         $("#datatable").dataTable({
             "columnDefs": [
                 { "sortable": true, "targets": [0] }
+            ],
+            'dom': "Bfrtip",
+            'buttons': [
+                'excel',
             ]
         });
     </script>
